fbb34b6e642dc61053df1c454b217d7966a64cf6
abbkell148 <abbkell@gmail.com>
2018-10-31 16:30:53 -0400

    Added a navbar at the top with two tabs. Second tab is empty. When there was stuff in it, it broke the connection between ui and server.

--- ExtinctionExploration/ui.R
+++ ExtinctionExploration/ui.R
@@ -7,28 +7,24 @@
 #    http://shiny.rstudio.com/
 #
 
-#library(shiny)
-
-
-
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Diversity and Extinction Through Time"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      checkboxGroupInput("group", "Taxa to plot", 
-                   choices = group_names),
-                   
-      checkboxInput('select','All/None')
-),
-    # Show a plot of the generated distribution
-    mainPanel(tabsetPanel(
-       tabPanel("Standardized Diversity", plotOutput("standard_diversity_plot")),
-       tabPanel("Raw Diversity", plotOutput("raw_diversity_plot"))
-    ))
-  )
-))
+shinyUI(navbarPage("Diversity and Extinction", 
+    tabPanel("Part1",
+      fluidPage(titlePanel("Diversity and Extinction Through Time"),
+        # Sidebar with a slider input for number of bins 
+        sidebarLayout(
+          sidebarPanel(
+            checkboxGroupInput("group", "Taxa to plot", choices = group_names),
+            checkboxInput('select','All/None')
+            ),
+          # Show a plot of the generated distribution
+          mainPanel(tabsetPanel(
+              tabPanel("Standardized Diversity", plotOutput("standard_diversity_plot")),
+              tabPanel("Raw Diversity", plotOutput("raw_diversity_plot"))
+              ))
+          )
+        )),
+    
+    tabPanel("Part2")
+)
+)
