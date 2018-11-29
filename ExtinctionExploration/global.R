bbaa16390c7686416fc2dfe148a8e36f138c1868
abbkell148 <abbkell@gmail.com>
2018-11-29 14:10:07 -0500

    Quick Fix!

--- ExtinctionExploration/global.R
+++ ExtinctionExploration/global.R
@@ -13,7 +13,7 @@ library(cowplot)
  
  
 # #group names for checkbox labels
- group_names <- c('Animals (all)','Ammonites',  'Arthropods', 'Arthropods: insects', 'Arthropods: trilobites', 'Brachiopods', 'Bryozoans', 'Corals (all)', 'Corals: rugose', 'Corals: scleractinian', 'Corals: tabulate', 'Echinoids (all)', 'Echinoids: crinoids', 'Echinoids: sea stars', 'Echinoids: sea urchins', 'Fish: bony', 'Fish: cartilaginous', 'Molluscs (all)', 'Molluscs: bivalves', 'Molluscs: gastropods')
+ group_names <- c('Animals (all)','Ammonites',  'Arthropods', 'Arthropods: insects', 'Arthropods: trilobites', 'Brachiopods', 'Bryozoans', 'Corals (all)', 'Corals: rugose', 'Corals: scleractinian', 'Corals: tabulate', 'Echinodermata (all)', 'Echinodermata: crinoids', 'Echinodermata: sea stars', 'Echinodermata: sea urchins', 'Fish: bony', 'Fish: cartilaginous', 'Molluscs (all)', 'Molluscs: bivalves', 'Molluscs: gastropods')
 
  hypotheses  <- levels(read.csv("./data/selectivity.csv")$comparison)
  
